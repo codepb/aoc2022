8ccc63fdce5885b33ee2a698d9f2ab697fe165b7
Paul Burgess <paul.j.burgess@outlook.com>
2022-12-04 14:57:43 +0000

    day 4

---
+++ src/answers/days/4/part1.ts
@@ -0,0 +1,15 @@
+import type RunnerFunction from "../../types/RunnerFunction.js";
+import { splitLines } from "../../utils/string.js";
+
+function isContained(a: [number, number], b: [number, number]) {
+    return (a[0] >= b[0] && a[1] <= b[1]) || (b[0] >= a[0] && b[1] <= a[1]);
+}
+
+const runner: RunnerFunction<number> = (input) => {
+    return splitLines(input).reduce((prev, curr) => {
+        const pair = curr.split(',').map(p => p.split('-').map(n => parseInt(n)));
+        return isContained(pair[0] as [number, number], pair[1] as [number, number]) ? prev + 1 : prev;
+    }, 0);
+}
+
+export default runner;
